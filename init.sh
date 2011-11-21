1844c46163de02d3d614c833b359a48c344d41b3
Hendrik Mans <hendrik.mans@tolingo.com>
2011-11-21 12:14:22 +0100

    Add experimental rbfu-exec. Probably needs a nicer syntax.

--- init.sh
+++ init.sh
@@ -17,6 +17,14 @@ function rbfu () {
   eval "\`$HOME/.rbfu/libexec/rbfu-activate \$1\`"
 }
 
+function rbfu-exec () {
+  previous_version=\$RBFU_RUBY_VERSION
+  new_version=\$1; shift
+  rbfu \$new_version 2>/dev/null
+  \$@
+  rbfu \$previous_version 2>/dev/null
+}
+
 [ -f $HOME/.rbfu-version ] && rbfu
 EOD
 
